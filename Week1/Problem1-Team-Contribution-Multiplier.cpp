c8915a85c7fc670ffc320d17689d0624f5ccb867
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2025-12-29 14:36:24 +0500

    Solved Problem 1 of Week 1

---
+++ Week1/Problem1-Team-Contribution-Multiplier.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+Asymptotic Analysis
+
+Time Complexity = O(N)
+Space Complexity = O(1)
+
+*/
+
+vector<int> solve(vector<int>& contributions) {
+    int n = contributions.size();
+    if(n == 0) {return {};}
+
+    vector<int> impact(n, 1);
+    int suf = contributions.back();
+    for(int i = n - 2; i >= 0; i--) {
+        impact[i] *= suf;
+        suf *= contributions[i];
+    }
+
+    int pre = 1;
+    for(int i = 0; i < n; i++) {
+        impact[i] *= pre;
+        pre *= contributions[i];
+    }
+
+    // Output
+    // for(int i = 0; i < n; i++) {cout << impact[i] << ' ';}
+    // cout << '\n';
+
+    return impact;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<int> contributions = {1, 2, 3, 4};
+    solve(contributions);
+
+    return 0;
+}
