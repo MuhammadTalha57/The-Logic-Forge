facc371a7fe297f7e3a9a48a826a6216e205d133
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2025-12-30 20:14:48 +0500

    Week 1 Progress: Optimized Algorithm for peoblem 6.

--- Week1/challenge6_pattern_matcher.cpp
+++ Week1/challenge6_pattern_matcher.cpp
@@ -5,10 +5,10 @@ using namespace std;
 /*
 Asymptotic Analysis
 
-Time Complexity = O( (N^2)M )
+Time Complexity = O(NM)
 Space Complexity = O(NM)
 
-Efficient for N, M <= 100
+Efficient for N, M <= O(10^3)
 
 */
 
@@ -34,20 +34,25 @@ bool solve(string& text, string& pattern) {
     // Define for [1][1]
     dp[1][1] = pattern[0] == '*' || pattern[0] == '?' || pattern[0] == text[0];
 
+    vector<int> columnCheck(m + 1, 0);
+    for(int j = 0; j <= m; j++) {
+        columnCheck[j] = dp[0][j];
+    }
+
     // Populate
     for(int i = 1; i <= n; i++) {
+        columnCheck[1] = columnCheck[1] || dp[i][1];
         for(int j = 2; j <= m; j++) {
             if(pattern[j - 1] == '?') {
                 dp[i][j] = dp[i - 1][j - 1];
 
             } else if(pattern[j - 1] == '*') {
-                for(int k = 0; k <= i; k++) {
-                    dp[i][j] = dp[i][j] || dp[i - k][j - 1];
-                }
+                dp[i][j] = columnCheck[j - 1];
 
             } else {
                 dp[i][j] = text[i - 1] == pattern[j - 1] && dp[i - 1][j - 1];
             }
+            columnCheck[j] = columnCheck[j] || dp[i][j];
         }
     }
 
@@ -59,8 +64,8 @@ int main() {
     cin.tie(0);
     cout.tie(0);
 
-    // vector<int> contributions = {1, 2, 3, 4};
-    // solve(contributions);
+    string text = "ab", pattern = "*a";
+    cout << solve(text, pattern);
 
 
     return 0;
